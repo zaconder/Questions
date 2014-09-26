82b75f5cb56f1ba30a629710b2e9fff9f7d4939b
zaconder <zac@conder.us>
2014-09-26 12:27:41 -0600

    Update

---
+++ src/Questions/controller/QuestionsRunner.java
@@ -0,0 +1,12 @@
+package Questions.controller;
+
+public class QuestionsRunner
+{
+
+	public static void main(String[] args)
+	{
+		Questions myQuestions = new Questions();
+		myQuestions.start();
+	}
+
+}
